8b4e21bfa3e9f26f9c66825f90da16f222760c95
gigo890 <149149857+gigo890@users.noreply.github.com>
2024-01-01 18:56:55 +0000

    commit

---
+++ footer.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class = "footer">
+        <ul>
+            <li><a href="index.php">About Us</a></li>
+            <li><a href="inded.php">Contact Us</a></li>
+        </ul>
+    </div>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class = "nav">
+        <ul>
+        <li><a href="index.php" class="active"><img src="images/logo.jpg" alt=""></a></li>
+        <div id="myLinks">
+            <li><a href="courses.php">Courses</a></li>
+            <li><a href="index.php">Facilities</a></li>
+            <li><a href="index.php">About Us</a></li>
+        </div>
+        <a href="javascript:void(0);" class="icon" onclick ="myFunction()">
+        <div class="bar1"></div>
+        <div class="bar2"></div>
+        <div class="bar3"></div>
+        </a>
+        </ul>
+    </div>
+</body>
+</html>
